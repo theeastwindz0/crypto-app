6b4043cae7a878f39fbc81fa6ea9e947121f97e8
Himanshu Chauhan <developerhimanshuchauhan@gmail.com>
2022-12-13 19:31:28 +0530

    Working on details screen

--- src/components/DetailsBid.js
+++ src/components/DetailsBid.js
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import assets from '../../constants/assets'
 
-const DetailsBid = () => {
+const DetailsBid = ({bid}) => {
   return (
-    <View>
-      <Text>DetailsBid</Text>
+    <View className='p-2 flex flex-row justify-between'>
+      <Image
+      source={bid.image}
+      resizeMode='contain'
+      className='w-12 h-12'
+      />
+
+      <View>
+        <Text className='font-semibold'>Bid Placed by {bid.name}</Text>
+        <Text>{bid.date}</Text>
+      </View>
+
+      <View className='flex flex-row justify-center items-center'>
+          <Image 
+          source={assets.eth} 
+          className='w-5 h-5'
+          resizeMode='contain'
+          />
+          <Text className='font-bold'>{bid.price}</Text>
+        </View>
     </View>
   )
 }
